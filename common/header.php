c5cf0d10837d88d9524fe2380314bd67326b5726
wbfprp <wbfprp@naver.com>
2023-01-18 08:51:49 +0900

    initial commit

---
+++ common/header.php
@@ -0,0 +1,25 @@
+<?php
+echo '<head>';
+echo '<meta charset="utf-8">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<meta name="description" content="">';
+echo '<meta name="author" content="">';
+echo '<link rel="shortcut icon" href="media/img/cross.png" type="image/x-icon">';
+echo '<title>Seongheon Hong</title>';
+echo '<link href="css/bootstrap.css" rel="stylesheet">';
+echo '<link href="css/main.css" rel="stylesheet">';
+echo '</head>';
+echo '';
+echo '<body>';
+echo '<div class="container">';
+echo '<hr style="border-top: 2px solid; opacity: 0.7;">';
+echo '<div class="row" style="text-align: center;">';
+echo '<div class="col-sm-3"><b>Home</b></div>';
+echo '<div class="col-sm-3"><a id="prj" href="projects.html" style="color: black;">Projects</a></div>';
+echo '<div class="col-sm-3"><a href="life.html" style="color: black;">Life</a></div>';
+echo '<div class="col-sm-3"><a href="gallery.html" style="color: black;">Gallery</a></div>';
+echo '</div>';
+echo '<hr style="border-top: 2px solid; opacity: 0.7;">';
+echo '</div>';
+echo '</body>';
+?>
